554405b0aadf1efdff5fbd88aa46de137ebf2276
Petar Cekic <cekic.petar9999@gmail.com>
2018-11-26 14:47:22 +0100

    add cordova advanced http / ionic http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP } from '@ionic-native/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
@@ -34,6 +35,7 @@ import { ComponentsModule } from '../components/components.module';
     TabsPage
   ],
   providers: [
+    HTTP,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
