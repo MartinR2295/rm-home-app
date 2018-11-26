f05aefb68e294566c6229c482b16274985c6befd
Martin <martinhlsl@yahoo.de>
2018-11-26 11:49:43 +0100

    add base, dark, light button, base view

---
+++ src/components/base/base-view/base-view.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the BaseViewComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  template: ''
+})
+export class BaseViewComponent {
+
+  constructor() {
+    
+  }
+
+}
