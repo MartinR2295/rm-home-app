567368aaf16a3e7f8c05a8ad7fff4b819198cb3f
Martin <martinhlsl@yahoo.de>
2018-11-29 15:38:27 +0100

    add login api logi

--- src/providers/base/base.ts
+++ src/providers/base/base.ts
@@ -10,26 +10,20 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class BaseProvider {
-  static readonly backpointURL = "rm-home.rmst.eu";
+  static readonly backpointURL = "https://rm-home.rmst.eu";
   constructor(public http: HTTP) {}
 
-  createAuthorizationHeader(headers: Headers) {
-    headers.append('rCBst3ERrvYYsj59C9PnN7VNMvvfgr4n54', 'KcUt52Q6JC9PVeAFkwh63946k8CBt6ss'); 
+  getHeaders() {
+    return {
+      "api-key": "KcUt52Q6JC9PVeAFkwh63946k8CBt6ss"
+    }
   }
 
   get(url, data) {
-    let headers = new Headers();
-    this.createAuthorizationHeader(headers);
-    return this.http.get(url, data, {
-      headers: headers
-    });
+    return this.http.get(url, data, this.getHeaders());
   }
 
   post(url, data) {
-    let headers = new Headers();
-    this.createAuthorizationHeader(headers);
-    return this.http.post(url, data, {
-      headers: headers
-    });
+    return this.http.post(url, data, this.getHeaders());
   }
 }
