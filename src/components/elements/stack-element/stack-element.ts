ff453db70b27d8fcf1ec9aa734d6ac55e917339e
Martin <martinhlsl@yahoo.de>
2018-12-07 11:42:38 +0100

    add stack-element

---
+++ src/components/elements/stack-element/stack-element.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the StackElementComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'stack-element',
+  templateUrl: 'stack-element.html'
+})
+export class StackElementComponent {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello StackElementComponent Component');
+    this.text = 'Hello World';
+  }
+
+}
