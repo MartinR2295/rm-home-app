41fb24dd548c82bb84cef443de06e827dbde337a
Martin <martinhlsl@yahoo.de>
2018-12-12 12:12:00 +0100

    fix object-detail bug

--- src/pages/object-detail/object-detail.ts
+++ src/pages/object-detail/object-detail.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RMHObjectModel } from '../../app/models/RMHObjectModel';
 import { InventoryModel } from '../../app/models/InventoryModel';
 import { ObjectProvider } from '../../providers/object/object';
+import { QRCodeModel } from '../../app/models/QRCodeModel';
 /**
  * Generated class for the ObjectDetailPage page.
  *
@@ -31,7 +32,7 @@ export class ObjectDetailPage {
     this.object = JSON.parse(this.navParams.get('object'));
 
     this.getContents(this.object.object_id);
-    this.getStack(this.object.object_id);
+    //this.getStack(this.object.object_id);
   }
 
   ionViewDidLoad() {
@@ -49,6 +50,8 @@ export class ObjectDetailPage {
       default:
         break;
     }
+
+    console.log("currentObjects: ", this.currentObjects)
   }
 
   clickObject(object) {
@@ -58,9 +61,29 @@ export class ObjectDetailPage {
   }
 
   getContents(id) {
-    this.objp.getObject(`objects/${id}/content`, null, this.objp.getHeaders())
+    console.log(' start contents: ');
+    this.objp.getObject(`objects/${id}/detail`, null, this.objp.getHeaders())
     .then((res) => {
-      this.content = JSON.parse(res.data).body;
+      console.log(' content then: ', res);
+
+      JSON.parse(res.data).body.content.forEach(element => {
+        var cObj:RMHObjectModel = element;
+        cObj.object_qr_code = new QRCodeModel();
+        cObj.object_qr_code.qr_code_string = cObj.qr_code_string;
+        this.content.push(cObj);
+      });
+      
+      var currentObject:RMHObjectModel = JSON.parse(res.data).body.stack;
+
+      while (currentObject.object_parent) {
+        this.stack.push(currentObject); 
+
+        if (currentObject.object_parent) {
+          currentObject = currentObject.object_parent;
+        }
+      }
+      this.stack.push(currentObject);
+      this.segmentChanged();
     })
     .catch((error) => {
       console.log('error getContents: ', error);
@@ -69,8 +92,8 @@ export class ObjectDetailPage {
   }
 
   getStack(id) {
+    console.log(' start stack: ');
     this.objp.getObject(`objects/${id}/stack`, null, this.objp.getHeaders())
-
     .then((res) => {
       console.log('stack: ', res);
 
